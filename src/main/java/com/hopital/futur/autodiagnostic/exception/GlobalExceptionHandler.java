f5bf265cc08c27cf76e54e6e778d84de0b699479
Moh Med <gmelk02@gmail.com>
2024-09-14 14:25:50 +0200

    Clean code

--- src/main/java/com/hopital/futur/autodiagnostic/exception/GlobalExceptionHandler.java
+++ src/main/java/com/hopital/futur/autodiagnostic/exception/GlobalExceptionHandler.java
@@ -2,8 +2,6 @@ package com.hopital.futur.autodiagnostic.exception;
 
 import com.hopital.futur.autodiagnostic.response.ErrorResponse;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
